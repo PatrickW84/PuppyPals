90f137c73ff02e00b4bc6e62e205c34582e4b0de
PatrickW84 <allenpatrickwilliams@gmail.com>
2024-06-12 13:12:50 -0500

    tabThree

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,36 @@ import { puppyList } from "./data.js";
 function App() {
   const [puppies, setPuppies] = useState(puppyList); //const[value you are storing, a function you can use to reset the value]
   //useState is a hook - it needs a default value -- here, the (puppyList) array, from the data.js is the default value
-  console.log("Puppy List: ", puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  function handleClick() {
+    // some logic here
+  }
+  // console.log("Puppy List: ", puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
   return (
     <>
-      <div>
+      <div className="App">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          ); //key maintains the integrity of the order of elements
         })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
